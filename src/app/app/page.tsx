0b59a7f02dd8f3fd874279357da5815f8b9ef3ac
plsgivemeachane <onenew553@gmail.com>
2024-01-23 08:52:56 +0700

    fuking conflict

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -204,7 +204,7 @@ export default function Home() {
   const [isLoaded, setLoaded] = useState(false);
   const [stats, setStats] = useState("");
   const [isMobile, setMobile] = useState(false);
-  const themeElement = useRef(null);
+  const themeElement = useRef<HTMLDivElement>();
 
   useEffect(() => {
     if(localStorage.getItem("dir") != null) setDirectory(localStorage.getItem("dir")!);
